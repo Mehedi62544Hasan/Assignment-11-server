db4d9d61e6fa3cdbc682b45cbfe15add032b38a5
Mehedi62544Hasane <mehedi62544hasan@gmail.comm>
2022-11-09 15:50:16 +0000

    get service review done

--- index.js
+++ index.js
@@ -48,7 +48,15 @@ async function run() {
             const review = req.body;
             const result = await reviewCollection.insertOne(review);
             res.send(result);
-        })
+        });
+
+        app.get('/review', async (req, res) => {
+            const query = {};
+            const cursor = reviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        });
+
  
     }
     finally {
