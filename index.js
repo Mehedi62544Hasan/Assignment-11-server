c399c6392fd094c9f0ead930cd8f839e1f9d7378
Mehedi62544Hasane <mehedi62544hasan@gmail.comm>
2022-11-10 01:40:59 +0000

    my review design done

--- index.js
+++ index.js
@@ -51,25 +51,24 @@ async function run() {
         });
 
         app.get('/review', async (req, res) => {
+             let query = {};
+                if(req.query.email){
+                    query = {
+                        email: req.query.email
+                    }
+                }
+            const cursor = reviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        })
+
+        app.get('/review', async (req, res) => {
             const query = {};
             const cursor = reviewCollection.find(query);
             const reviews = await cursor.toArray();
             res.send(reviews);
         });
 
-        
-
-        app.get('/review', async (req, res) => {
-            let query = {};
-            if (req.query.email) {
-                query = {
-                    email: req.query.email
-                }
-            }
-            const cursor = reviewCollection.find(query);
-            const orders = await cursor.toArray();
-            res.send(orders);
-        })
  
     }
     finally {
